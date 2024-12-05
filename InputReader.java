6f67efc114706f681fd8caa225b10cfe32520555
Lirik Salihu <G00304223@stu.raritanval.edu>
2024-12-04 21:33:53 -0500

    Update InputReader.java

--- InputReader.java
+++ InputReader.java
@@ -6,8 +6,8 @@ import java.util.Scanner;
  * The text typed by a user is then chopped into words, and a set of words 
  * is provided.
  * 
- * @author David J. Barnes and Michael Kölling.
- * @version 2016.02.29
+ * @author Lirik Salihu
+ * @version 2024.12.04
  */
 public class InputReader
 {
